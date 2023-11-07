db79de57d402c860f7a2758ec97de1ecfc5973ac
geusan <dnay2k@gmail.com>
2023-11-07 19:52:25 +0900

    fix: error

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isGithub = process.env.BUILD_ENV === 'github';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isGithub ? '/event.poomang.com/' : undefined,
-  assetPrefix: isGithub ? '/event.poomang.com/' : undefined,
+  basePath: isGithub ? '/event.poomang.com' : undefined,
+  assetPrefix: isGithub ? '/event.poomang.com' : undefined,
 }
 
 module.exports = nextConfig
