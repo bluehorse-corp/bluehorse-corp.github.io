362a1a9805aef1cc79b2d8f1c71c5bd5bf133026
geusan <dnay2k@gmail.com>
2023-11-07 20:49:35 +0900

    fix: github workflow

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-// const isGithub = process.env.BUILD_ENV === 'github' || true;
+const isGithub = process.env.BUILD_ENV === 'github' || true;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   // basePath: isGithub ? '/event.poomang.com' : undefined,
-  // assetPrefix: isGithub ? '/event.poomang.com' : undefined,
+  assetPrefix: isGithub ? '/event.poomang.com/' : undefined,
 }
 
 module.exports = nextConfig
