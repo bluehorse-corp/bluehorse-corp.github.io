062d674dc376460bf8397f41fa81c7d1bd52c72b
geusan <dnay2k@gmail.com>
2023-11-07 19:50:03 +0900

    fix: add basePath in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const isGithub = process.env.BUILD_ENV === 'github';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: isGithub ? 'https://bluehorse-corp.github.io/event.poomang.com/' : undefined,
+  basePath: isGithub ? '/event.poomang.com/' : undefined,
+  assetPrefix: isGithub ? '/event.poomang.com/' : undefined,
 }
 
 module.exports = nextConfig
