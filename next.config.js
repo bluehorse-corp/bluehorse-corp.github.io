75157a1ef4e484c32373a78693c13bea413da023
geusan <dnay2k@gmail.com>
2023-11-07 18:58:35 +0900

    add: asset Prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isGithub = process.env.BUILD_ENV === 'github';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  assetPrefix: isGithub ? 'https://bluehorse-corp.github.io/event.poomang.com/' : undefined,
 }
 
 module.exports = nextConfig
